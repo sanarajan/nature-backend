6766192b3d33dfc543e3d9e0025a75fa24a1ea29
sanarajan <sanamol87@gmail.com>
2026-03-13 17:26:08 +0530

    remove env, dist, node_modules and add gitignore

--- dist/interface/routes/user/categoryRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const CategoryController_1 = require("../../controllers/CategoryController");
-const router = (0, express_1.Router)();
-router.get('/', CategoryController_1.getCategoriesWithCounts);
-router.get('/hierarchy', CategoryController_1.getCategoryHierarchy);
-exports.default = router;
--- dist/interface/routes/user/productRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const ProductController_1 = require("../../controllers/ProductController");
-const router = (0, express_1.Router)();
-router.get('/', ProductController_1.getFilteredProducts);
-router.get('/featured', ProductController_1.getFeaturedProducts);
-router.get('/popular', ProductController_1.getPopularProducts);
-exports.default = router;
